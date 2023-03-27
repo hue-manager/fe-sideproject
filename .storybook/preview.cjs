9eb9b67d8861fd17034631f1dd0307fcd504cb7f
jisooround <9utesleben@gmail.com>
2023-03-27 10:24:18 +0900

    Design: Styled Component 적용

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,20 @@
+import React from 'react'
+import { StylesProvider } from '@material-ui/core'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from '@/styles/GlobalStyle'
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme="default">
+      <StylesProvider injectFirst>
+        <GlobalStyle />
+        <Story />
+      </StylesProvider>
+    </ThemeProvider>
+  ),
+]
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,

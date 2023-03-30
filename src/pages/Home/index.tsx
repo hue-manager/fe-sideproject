d1fcd24a2683b0913e3d6ffe4cccb1cf61fd202a
jisooround <9utesleben@gmail.com>
2023-03-31 01:55:07 +0900

    Test: 임시 로그아웃 기능

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,25 @@
+import { logout } from '../../api/auth'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {}
 
 const Home = (props: Props) => {
-  return <div>Home</div>
+  const navigate = useNavigate()
+
+  const logoutSubmitHandler = async (event: React.MouseEvent) => {
+    event.preventDefault()
+    const res = await logout()
+    if (res) {
+      navigate('/')
+    }
+  }
+  return (
+    <div>
+      Home
+      <button onClick={logoutSubmitHandler}>임시 로그아웃 버튼</button>
+    </div>
+  )
 }
 
 export default Home

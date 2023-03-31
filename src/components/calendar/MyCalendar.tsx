c5eaf5b17a23d9d765b8e5b0a080a32e44e5f9db
Hyeeeein <khi_9866@naver.com>
2023-03-31 23:31:22 +0900

    Design: 캘린더 추가

---
+++ src/components/calendar/MyCalendar.tsx
@@ -0,0 +1,376 @@
+import React, { useEffect, useState } from 'react'
+import { Calendar, momentLocalizer, DateLocalizer, Views } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Header } from '../../stories/Header'
+import './MyCalendar.css'
+import styled from 'styled-components'
+import { within } from '@storybook/testing-library'
+
+const MyDateHeader = ({ label, date }: { label: string; date: string }) => {
+  return (
+    <div>
+      <button className="rbc-button-link" role="cell">
+        {label}
+      </button>
+    </div>
+  )
+}
+
+const MyCalendar = () => {
+  const localizer = momentLocalizer(moment)
+
+  const events = [
+    {
+      title: 'My Event',
+      start: new Date('2023-03-4'),
+      end: new Date('2023-03-10'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-4'),
+      end: new Date('2023-03-4'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-4'),
+      end: new Date('2023-03-4'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-4'),
+      end: new Date('2023-03-4'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'My Event',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      // resource: (event: any) => console.log(event),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'sdkfls',
+      start: new Date('2023-03-13'),
+      end: new Date('2023-03-13'),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: '니아러니알',
+      start: new Date('2023-03-15'),
+      end: new Date('2023-03-15'),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+    {
+      title: 'ㄴㅇㄹㄴㅇ',
+      start: new Date('2023-03-15'),
+      end: new Date('2023-03-15'),
+      data: {
+        type: '연차',
+        name: '김민지',
+        affiliation: '디자인팀',
+        position: '주임',
+      },
+    },
+  ]
+
+  const test = (event: Object, start: Date, end: Date, isSelected: boolean): any => {
+    // console.log(event)
+    // console.log(start)
+    // console.log(Date)
+    // console.log(isSelected)
+  }
+
+  const component = {
+    // event: (props: any) => {
+    //   // console.log(props)
+    //   return null
+    // },
+    month: {
+      event: (props: any) => {
+        // console.log('month: ', props)
+        return <span className="test"></span>
+      },
+    },
+    // day: {
+    //   event: (props: any) => {
+    //     console.log(props)
+    //     return null
+    //   },
+    // },
+  }
+
+  return (
+    <Calendar
+      components={component}
+      tooltipAccessor={(): any => null}
+      eventPropGetter={test}
+      events={events}
+      localizer={localizer}
+      defaultView={'month'}
+      selectable={false}
+      showAllEvents={true}
+      messages={{
+        previous: '<',
+        next: '>',
+        today: 'today',
+
+        showMore: (total) => `+${total} more`,
+      }}
+      views={['month']} // 뭘 보여줄지 month, day 등
+      // date={moment('2023-01-15').toDate()} // 해당 날짜로 이동
+      formats={{
+        monthHeaderFormat: (date: Date) => moment(date).format('M'),
+        dateFormat: (date: Date) => moment(date).format('D'),
+      }}
+      // onSelectEvent={test} // 해당 이벤트를 클릭했을 때 옆에 목록 보여주면 될 듯
+      // onSelectSlot={test} // 해당 일 슬롯을 클릭했을 때 그 슬롯의 정보를 참조
+      // selectable={false} // onSelectSlot 할 때 꼭 넣어줘야 함
+      onShowMore={(events, date) => console.log(events, date)} // 더보기 눌렀을 때 정보 더 나오는 것
+      // toolbar={false}
+      popup={true} // 이벤트 더보기 눌렀을 때 밑에 팝업 안 뜨게
+      startAccessor="start"
+      endAccessor="end"
+      // getNow={() => new Date('2023-04-13')} // 오늘을 가져오는 듯?
+      titleAccessor="title"
+      style={{ height: 688, width: 441 }}
+    />
+  )
+}
+
+export default MyCalendar
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import TotalCalendar from './../../components/calendar/TotalCalendar'
 
 interface Props {}
 
 const Home = (props: Props) => {
-  return <div>Home</div>
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <TotalCalendar />
+    </div>
+  )
 }
 
 export default Home

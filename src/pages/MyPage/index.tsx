fe9de9dbb72238c0f910e46cce0238d7131e87ce
jisooround <9utesleben@gmail.com>
2023-04-01 10:56:33 +0900

    Test: 타이머

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import Avatar, { genConfig } from 'react-nice-avatar'
 import Content from '@components/Content'
 import { Button } from '@components/Button/Button'
+import TimeStamp from '@components/timeStamp/TimeStamp'
+import { getCurrent } from '../../../src/utils/timer'
 
 interface Props {}
 
 const MyPage = (props: Props) => {
   const config = genConfig('qwer1234@qwer1234.com')
+
+  console.log(getCurrent())
   return (
     <Page>
       <Sidebar />
+      <TimeStamp></TimeStamp>
       <Container>
         <Content title={'내 정보'} intro={'내 정보를 확인하고 수정할 수 있습니다.'}>
           <Profile>

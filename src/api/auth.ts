8171566f762babc83c7c95586ef610da68c50f67
jisooround <9utesleben@gmail.com>
2023-04-02 11:39:16 +0900

    Feat: 관리자 로그인 api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,43 +2,56 @@ import { removeToken, setExpiration, setToken } from '../utils/cookies'
 import API_URLS from '../constants/apiConst'
 import instance from './apiController'
 
+// 일반 유저 로그인
 export const login = async (email: string, password: string) => {
   try {
     const response = await instance.post(API_URLS.LOGIN, {
       email: email,
       password: password,
     })
-    if (response.data.message === '존재하지 않는 이메일 입니다.') {
-      console.log('fail')
-      return 'fail'
+    if (response.data.message === '비밀번호를 확인해주세요.') {
+      console.log(response.data.message)
+      return 'wrong assword'
     }
+    // 로그인 성공시에 토큰과 토큰 삭제시간 쿠키 저장소에 저장
     if (response.data.message === '로그인 성공') {
-      console.log({ token: response.data })
-      setToken(response.data)
+      console.log(response.data)
+      setToken(response.data.token)
       setExpiration()
       return response
     }
   } catch (error) {
     if (error instanceof Error) {
-      console.log(error.message)
+      return 'fail'
     } else {
       throw error
     }
   }
 }
 
-export const logout = async () => {
+export const loginAdmin = async (email: string, password: string) => {
   try {
-    const response = await instance.post(API_URLS.LOGOUT)
-    console.log('로그아웃 성공')
-    removeToken()
-    console.log('삭제 완료')
-    return response
+    const response = await instance.post(API_URLS.LOGIN_ADMIN, {
+      email: email,
+      password: password,
+    })
+    if (response.data.message === '비밀번호를 확인해주세요.') {
+      console.log(response.data.message)
+      return 'wrong assword'
+    }
+    // 로그인 성공시에 토큰과 토큰 삭제시간 쿠키 저장소에 저장
+    if (response.data.message === '로그인 성공') {
+      setToken(response.data.token)
+      setExpiration()
+      return response
+    }
   } catch (error) {
     if (error instanceof Error) {
-      console.log(error.message)
+      return 'fail'
     } else {
       throw error
     }
   }
 }
+
+export const logout = () => {}

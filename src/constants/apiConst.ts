9b5373f8a2e9aeef550857afc945f63f1e42b6de
jisooround <9utesleben@gmail.com>
2023-04-03 06:45:45 +0900

    Feat: 회원가입 api 호출

--- src/constants/apiConst.ts
+++ src/constants/apiConst.ts
@@ -2,8 +2,10 @@ const API_URLS = {
   LOGIN: '/login',
   LOGOUT: '/logout',
   LOGIN_ADMIN: '/admins/login',
-  USER_INFO: '/users/',
+  USER_INFO: /users/,
   SAVE: '/schedules/save',
+  SIGNUP: '/signup',
+  EDIT_INFO: `/editinfo`,
 }
 
 export default API_URLS
